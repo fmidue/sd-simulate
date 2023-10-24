5bfd43adc0f5d1f24b3338ce5fcbec9cd7665121
Haya Zada <hayazada@Hayas-MacBook-Air.local>
2023-10-24 15:47:29 +0200

    nestellevelretrial

--- src/BA.py
+++ src/BA.py
@@ -6,19 +6,19 @@ import tempfile
 import xml.etree.ElementTree as ET
 
 ELEMENTS = []
+STATE_HIERARCHY = {}
+
+color_mode = True
+
+svg_file_path = None
 
 
 def parse_svg(file_path):
     tree = ET.parse(file_path)
     root = tree.getroot()
-
+    result_list = []
     text_elements = root.findall(".//{http://www.w3.org/2000/svg}text")
     rect_elements = root.findall(".//{http://www.w3.org/2000/svg}rect")
-
-    result_list = []
-
-    state_hierarchy = {}
-
     for text_element in text_elements:
         text_fill_color = text_element.get("fill")
         if text_fill_color != "#000000":
@@ -34,51 +34,73 @@ def parse_svg(file_path):
                 rect_y = float(matching_rect.get("y"))
                 rect_width = float(matching_rect.get("width"))
                 rect_height = float(matching_rect.get("height"))
-
                 x1 = rect_x
                 x2 = rect_x + rect_width
                 y1 = rect_y
                 y2 = rect_y + rect_height
-
                 result_list.append((state_name, (x1, x2, y1, y2)))
+    result_list.sort(key=lambda x: (len(STATE_HIERARCHY.get(x[0], [])), x[1][0]))
+    STATE_HIERARCHY.update(build_state_hierarchy(result_list))
 
-                for other_state, (ox1, ox2, oy1, oy2) in result_list:
-                    if (
-                        x1 >= ox1 and x2 <= ox2 and y1 >= oy1 and y2 <= oy2
-                    ) and (state_name != other_state):
-                        state_hierarchy[state_name] = other_state
-
-    result_list.sort(key=lambda x: state_hierarchy.get(x[0], x[0]))
+    for state, hierarchy in STATE_HIERARCHY.items():
+        print(f"State: {state}")
+        print(f"Children: {hierarchy}")
+        print()
 
     global ELEMENTS
     ELEMENTS = result_list
 
 
-def render_uml_diagram(canvas, svg_file_path):
-    canvas.delete("all")
+def build_state_hierarchy(elements):
+    hierarchy = {}
+    stack = []
 
-    if not svg_file_path:
-        print("No SVG file selected.")
+    for state_name, (x1, x2, y1, y2) in elements:
+        hierarchy[state_name] = []
+
+        if stack:
+            for parent_name, (px1, px2, py1, py2) in reversed(stack):
+                if x1 >= px1 and x2 <= px2 and y1 >= py1 and y2 <= py2:
+                    hierarchy[parent_name].append(state_name)
+                    stack.append((state_name, (x1, x2, y1, y2)))
+                    break
+        else:
+            stack.append((state_name, (x1, x2, y1, y2)))
+
+    return hierarchy
+
+
+def no_colors_diagram(svg_content):
+    root = ET.fromstring(svg_content)
+    text_elements = root.findall(".//{http://www.w3.org/2000/svg}text")
+    rect_elements = root.findall(".//{http://www.w3.org/2000/svg}rect")
+    line_elements = root.findall(".//{http://www.w3.org/2000/svg}line")
+    for text_element in text_elements:
+        text_element.set("fill", "#000000")
+    for element in rect_elements + line_elements:
+        style = element.get("style")
+        style = style.replace("stroke:#", "stroke:#000000;")
+        element.set("style", style)
+    modified_svg_content = ET.tostring(root, encoding="unicode")
+    return modified_svg_content
+
+
+def toggle_color_mode():
+    global color_mode, svg_file_path
+    color_mode = not color_mode
+    if svg_file_path:
+        render_uml_diagram(canvas, svg_file_path)
+
+
+def on_canvas_click(event):
+    if not ELEMENTS:
         return
-
-    png_data = cairosvg.svg2png(url=svg_file_path)
-
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp_png:
-        temp_png.write(png_data)
-
-    image = Image.open(temp_png.name)
-    photo = ImageTk.PhotoImage(image)
-
-    canvas.create_image(0, 0, anchor=tk.NW, image=photo)
-    canvas.image = photo
-
-
-def on_canvas_click(event, root, canvas):
     x, y = event.x, event.y
-    check_state(x, y, root, canvas)
+    state_name = check_state(x, y)
+    show_popup(state_name, x, y)
 
 
-def check_state(x, y, root, canvas):
+def check_state(x, y):
     for element in reversed(ELEMENTS):
         x1, y1, x2, y2 = (
             element[1][0],
@@ -87,22 +109,54 @@ def check_state(x, y, root, canvas):
             element[1][3],
         )
         if x1 <= x <= x2 and y1 <= y <= y2:
-            show_popup(element[0], x, y, root)
-            break
-    else:
-        show_popup("Outside", x, y, root)
+            return element[0]
+    return "Outside"
 
 
-def show_popup(message, x, y, root):
-    popup = tk.Toplevel(root)
+def show_popup(message, x, y):
+    popup = tk.Toplevel()
     popup.title("Information")
-    label_coords = tk.Label(
-        popup, text=f"Clicked Coordinates (x, y): ({x}, {y})"
-    )
+    label_coords = tk.Label(popup, text=f"Clicked Coordinates (x, y): ({x}, {y})")
     label_state = tk.Label(popup, text=f"State: {message}")
     label_coords.pack()
     label_state.pack()
-    popup.mainloop()
+
+
+def render_uml_diagram(canvas, svg_file_path):
+    canvas.delete("all")
+    if not svg_file_path:
+        print("No SVG file selected.")
+        return
+    with open(svg_file_path, "r") as svg_file:
+        svg_content = svg_file.read()
+    if color_mode:
+        modified_svg_content = svg_content
+    else:
+        modified_svg_content = no_colors_diagram(svg_content)
+    png_data = cairosvg.svg2png(bytestring=modified_svg_content)
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp_png:
+        temp_png.write(png_data)
+    image = Image.open(temp_png.name)
+    photo = ImageTk.PhotoImage(image)
+    canvas.create_image(0, 0, anchor=tk.NW, image=photo)
+    canvas.image = photo
+
+
+def choose_file():
+    global svg_file_path
+    file_path = filedialog.askopenfilename(filetypes=[("SVG files", "*.svg")])
+    if not file_path:
+        return
+    print("Selected File:", file_path)
+    canvas.delete("all")
+    ELEMENTS.clear()
+    STATE_HIERARCHY.clear()
+    parse_svg(file_path)
+    max_x = max(state[1][1] for state in ELEMENTS)
+    max_y = max(state[1][3] for state in ELEMENTS)
+    canvas.config(width=max_x + 20, height=max_y + 20)
+    svg_file_path = file_path
+    render_uml_diagram(canvas, file_path)
 
 
 app = tk.Tk()
@@ -110,29 +164,15 @@ app.title("UML Diagram Viewer")
 
 canvas = tk.Canvas(app, bg="white", width=600, height=600)
 canvas.pack(expand=tk.YES, fill=tk.BOTH)
-canvas.bind("<Button-1>", lambda event: on_canvas_click(event, app, canvas))
-
-
-def choose_file():
-    file_path = filedialog.askopenfilename(filetypes=[("SVG files", "*.svg")])
-
-    if not file_path:
-        return
-
-    print("Selected File:", file_path)
-
-    canvas.delete("all")
-    ELEMENTS.clear()
-    parse_svg(file_path)
-
-    max_x = max(state[1][1] for state in ELEMENTS)
-    max_y = max(state[1][3] for state in ELEMENTS)
-    canvas.config(width=max_x + 20, height=max_y + 20)
-
-    render_uml_diagram(canvas, file_path)
-
 
 load_button = tk.Button(app, text="Load UML Diagram", command=choose_file)
 load_button.pack()
+
+toggle_button = tk.Button(
+    app, text="Switch Color Mode", command=toggle_color_mode
+)
+toggle_button.pack()
+
+canvas.bind("<Button-1>", on_canvas_click)
 
 app.mainloop()
