6cfa06efc434f8a7f7014dd353a8a2db232a9d5e
hayazada <haya.zada@stud.uni-due.de>
2023-11-20 05:30:08 +0100

    isort linter fix

--- src/GUI.py
+++ src/GUI.py
@@ -1,10 +1,10 @@
+import logging
 import os
 import re
 import tkinter as tk
 import tkinter.simpledialog
 import xml.etree.ElementTree as ET
 from tkinter import PhotoImage, filedialog, messagebox
-import logging
 
 import cairosvg
 from svg_parser import (
